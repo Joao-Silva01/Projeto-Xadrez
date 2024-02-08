c137cd4b552885c8a19fdd6bd51f695465020319
João Roberto <futurepromise12@gmail.com>
2024-02-08 11:21:29 -0300

    Tratando error com excecao personalizada

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -12,15 +12,43 @@
             this.colunas = colunas;
             pecas = new Peca[linhas,colunas]; 
         }
+
+        //Métodos
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha,coluna];
+        }
+        public  Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return peca(pos) != null;
         }
 
         public void ColocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha > linhas|| pos.coluna < 0 || pos.coluna>colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
